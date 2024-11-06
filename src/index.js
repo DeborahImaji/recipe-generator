d07d0f07561efaee043d2d5f191025ca026418eb
Deborah Imaji <deborahoimaji@gmail.com>
2024-11-06 09:41:35 +0100

    Added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 
-function generateRecipe(event) {
-    event.preventDefault();
-
+function displayRecipe(response) {
+    console.log("Recipe generated")
     new Typewriter("#recipe", {
-        strings: "Here is a pepperoni pizza recipe",
+        strings: response.data.answer,
         autoStart: true,
         delay: 1,
         cursor: "",
     });
 }
 
+function generateRecipe(event) {
+    event.preventDefault();
+
+    let instructionsInput = document.querySelector("#user-instructions");
+
+    let apiKey = "5c8o643ae4ebb3ft8e1b94e99eafcfb0";
+    let prompt = `User instructions: Generate a recipe about ${instructionsInput.value}`;
+    let context = `You are a culinary expert and you love to write short recipes. Your mission is to generate a simple recipe in basic HTML. Please do not display any HTML signs. Make sure to follow the user instructions. At the end of the recipe, please sign Deborah x SheCodes AI inside a <strong>`;
+
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    console.log("Generating recipe...");
+    console.log(`Prompt: ${prompt}`);
+    console.log(`Context: ${context}`);
+
+    axios.get(apiUrl).then(displayRecipe);
+}
+
 let recipeFormElement = document.querySelector("#recipe-generator-form");
 recipeFormElement.addEventListener("submit", generateRecipe);
+
+// 11:17
